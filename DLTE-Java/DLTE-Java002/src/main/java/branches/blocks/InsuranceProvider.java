b32aa2785c9e4ad24feeccd29080d9caa268c994
Nishmitha0904 <nishmithashetty2002@gmail.com>
2024-02-22 15:02:38 +0530

    Task #267 uploaded

--- DLTE-Java/DLTE-Java002/src/main/java/branches/blocks/InsuranceProvider.java
+++ DLTE-Java/DLTE-Java002/src/main/java/branches/blocks/InsuranceProvider.java
@@ -1,4 +1,34 @@
 package branches.blocks;
 
+import java.util.Scanner;
+
 public class InsuranceProvider {
+    public static void main(String[] args) {
+        String[] insure = {"Health", "Home", "Accidental", "Disability"};
+        String[] abc = {"Accidental", "Travel", "General"};
+        String[] inr = {"Home", "Health", "Vehicle", "Fire", "General"};
+
+        String insuranceType;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the type of insurance that you want");
+        insuranceType = scanner.nextLine();
+
+        for (String each : insure) {
+            if (each.equals(insuranceType)) {
+                System.out.println("Insure");
+            }
+        }
+        for (String each : abc) {
+            if (each.equals(insuranceType)) {
+                System.out.println("ABC");
+            }
+        }
+        for (String each : inr) {
+            if (each.equals(insuranceType)) {
+                System.out.println("INR");
+            }
+        }
+
+
+    }
 }
---
+++ DLTE-Java/DLTE-Java002/src/main/java/branches/blocks/Sample.java
@@ -0,0 +1,7 @@
+package branches.blocks;
+
+public class Sample {
+    public static void main(Integer[] args) {
+        System.out.println("Hello world");
+    }
+}
