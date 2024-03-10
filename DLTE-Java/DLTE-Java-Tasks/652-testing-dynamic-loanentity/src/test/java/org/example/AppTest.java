f47ce93200864fef7f78d24ccc8b38b955a7e5db
Nishmitha0904 <nishmithashetty2002@gmail.com>
2024-03-10 15:49:01 +0530

    Task #652 added

--- DLTE-Java/DLTE-Java-Tasks/652-testing-dynamic-loanentity/src/test/java/org/example/AppTest.java
+++ DLTE-Java/DLTE-Java-Tasks/652-testing-dynamic-loanentity/src/test/java/org/example/AppTest.java
@@ -1,45 +1,50 @@
 package org.example;
 
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    static String testFilePath = "loans.doc";
-    static List<Loan> loanList;
 
-    MyBankLoans myBankLoans = new MyBankLoans();
-
-
-    public static void initialize() {
-        loanList = Stream.of(
-                new Loan(37463782339867L, 80000.0, new Date(2021, 05, 10), "Closed", "Lauren", 7687345243L),
-                new Loan(65478366452377L, 70000.0, new Date(2024, 02, 20), "Open", "Han", 8974563726L),
-                new Loan(674563872634L, 150000.0, new Date(2023, 7, 2), "Closed", "Alice", 9874563785L),
-                ).collect(Collectors.toList());
-        try {
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(testFilePath));
-            objectOutputStream.writeObject(loanList);
-            objectOutputStream.close();
-        } catch (IOException ioException) {}
+    @Before
+    public void initialize() {
+        MyBankLoans myBankLoans = new MyBankLoans();
     }
 
-    public void testFetch() {
-        assertSame("open", myBankLoans.checkClosedLoans());
+    @Test
+    public void testAddNewLoan() {
+        MyBankLoans myBankLoans = new MyBankLoans();
+        Loan loan = new Loan(345437856746L, 50000.0, new Date(2024, 13, 9), "open", "Nishmitha", 98764563746L);
+        myBankLoans.loanList.add(loan);
+
+        assertSame("open", myBankLoans.loanList.get(0).getLoanStatus());
+        assertEquals(loan.getLoanAmount(), myBankLoans.loanList.get(0).getLoanAmount());
     }
+
+    
+
+    @Test (timeout = 500)
+    public void testAddLoan() {
+        MyBankLoans myBankLoans = new MyBankLoans();
+        Loan loan = new Loan(345437856746L, 50000.0, new Date(2024, 13, 9), "open", "Nishmitha", 98764563746L);
+        myBankLoans.loanList.add(loan);
+
+        assertFalse(myBankLoans.loanList.contains(loan));
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testException() {
+        testAddLoan();
+    }
+
+
 }
